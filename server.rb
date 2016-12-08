816402745adeb4d13d69e0795872f12fdb764ba7
Your Name <arssonist@yahoo.com>
2016-12-08 14:39:31 -0500

    add routes to render basic views. Added view files dev.

--- server.rb
+++ server.rb
@@ -4,3 +4,23 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/index' do
+  erb :index
+end
+
+get '/contact' do
+  erb :contact
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/project1' do
+  erb :project1
+end
+
+get '/project2' do
+  erb :project2
+end
