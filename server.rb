ffb94cc25ece81d36fad89cfc8dede1518521ce1
Your Name <arssonist@yahoo.com>
2016-12-12 10:51:29 -0500

    add footer styles and content for pages info

--- server.rb
+++ server.rb
@@ -2,6 +2,12 @@
 require 'sinatra'
 
 get '/' do
+  @my_email = "drumgod101@gmail.com"
+  @my_github = "https://github.com/arssonist/"
+  @my_twitter = "https://twitter.com/The_Seperator"
+  @my_phone = "306-519-0138"
+  @my_blog = "https://chrisdelwebdev.wordpress.com/"
+  @my_city = "Toronto, ON"
   erb :index
 end
 
