a86d9179105ea105991ebe8fb16052338baed2c1
Your Name <arssonist@yahoo.com>
2016-12-29 08:15:20 -0500

    Title: Adjust CSS in views, add Handlebars code Body: Fixed CSS placement in views so it is pulled in, div can be inside or outside of 'chunk' tags. Created Handlebars section, but template is not being returned as a string.

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,8 +1,27 @@
 $(document).ready(function(){
-  $(window).load(function(){
-    alert("test")
-  })
-  //   $('.button').on('click',function(){
-  // alert('test')
-  // })
-})
+  $('#work-details').click(function(){
+    // alert("test");
+    $.ajax({
+      url:"js/my_work.json",
+      method:'GET',
+      data:{},
+      dataType:'json'
+    }).done(function(response){
+      // console.log(response)
+// // Get the html from template in a string
+      var workTemplateString = $('#work-template').html();
+      console.log(workTemplateString)
+// // Compile the string into a function
+      var workTemplate = Handlebars.compile(workTemplateString);
+      console.log(workTemplate)
+// // Input response into the function
+      var output = workTemplate(response)
+      console.log(output)
+
+//
+//       $('#work-details').html(output);
+    });
+
+  });
+
+});
