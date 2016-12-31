c8008952cb0a67d968d70f7900acff31f4ae599d
Your Name <arssonist@yahoo.com>
2016-12-31 17:18:13 -0500

    title:trying to target only one single page's id; body: TRIED EVERYTHING! Selecting by id fires even when on other pages; cannot use .load or .ready. Finally was able to test if id was present with "if ($('#hero-index').length){}". The code executes now only on the index page.

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,23 @@
 $(document).ready(function(){
-  $('.hero-index').on('click', function(){
-    alert('test')
-    // $('.main').css({ "background-color":"yellow"})
-  })
+  if ($('#hero-index').length){
+    $('.hero').css({ "background-image":"none"})
+       $('.hero').css({ "background-color":"#222"})
+       $('.main').css({ "background-color":"#222"})
+  }
+
+
+
+
+// function indexChange(){
+//   // $('.hero').css({ "background-image":"none"})
+//   // $('.hero').css({ "background-color":"#222"})
+//   // $('.main').css({ "background-color":"#222"})
+// }
+
+
+// $('.off-canvas-wrapper').css({ "background-image":"none"})
+// $('.off-canvas-wrapper').css({ "background-color":"#222"})
+    // })
   //
   // $('.index-main').css({ "background-color":"yellow"})
   // $('.index-main').css({ "height":"100px"})
