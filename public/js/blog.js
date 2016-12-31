967984994cd5751283a4f4ba2a7d0678ce1c90b0
Your Name <arssonist@yahoo.com>
2016-12-30 22:38:50 -0500

    add google fonts abel, orbitron, open-sans

--- public/js/blog.js
+++ public/js/blog.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
   var blogTemplateString = $('#blog-template').html();
   // console.log(workTemplateString)
 // // // 2.Compile the string into a function
-  var workTemplate = Handlebars.compile(workTemplateString);
+  var blogTemplateRenderer = Handlebars.compile(blogTemplateString);
 //       // console.log(workTemplate)
 // // // 3.Input response into the function
-  var output = workTemplate(response)
+  var output = blogTemplateRenderer()
   // console.log(output)
 //
 // //4.append to page
