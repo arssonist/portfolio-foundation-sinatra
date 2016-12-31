e9dc9aaf249fc5aaf17e86bfb09369677757a66a
Your Name <arssonist@yahoo.com>
2016-12-31 01:22:18 -0500

    Title: Trying to change background image Body: On branch; Unable to access class for CSS; cannot use JQ outside of layout; no JS function available for getting the background image; Either need to redo the classes for CSS or write a function in vanilla JS that will change the image to :none on load"

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-  var blogTemplateString = $('#blog-template').html();
-  // console.log(workTemplateString)
-// // // 2.Compile the string into a function
-  var blogTemplateRenderer = Handlebars.compile(blogTemplateString);
-//       // console.log(workTemplate)
-// // // 3.Input response into the function
-  var output = blogTemplateRenderer()
-  // console.log(output)
-//
-// //4.append to page
-  $('#work-details').html(output);
+//   var blogTemplateString = $('#blog-template').html();
+//   // console.log(workTemplateString)
+// // // // 2.Compile the string into a function
+//   var blogTemplateRenderer = Handlebars.compile(blogTemplateString);
+// //       // console.log(workTemplate)
+// // // // 3.Input response into the function
+//   var output = blogTemplateRenderer()
+//   // console.log(output)
+// //
+// // //4.append to page
+//   $('#work-details').html(output);
 
 });
---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.hero-index').on('click', function(){
+    alert('test')
+    // $('.main').css({ "background-color":"yellow"})
+  })
+  //
+  // $('.index-main').css({ "background-color":"yellow"})
+  // $('.index-main').css({ "height":"100px"})
+});
