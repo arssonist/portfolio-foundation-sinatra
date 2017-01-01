214b9fe3db02c4954a8104488cce3ea81b51abe3
Your Name <arssonist@yahoo.com>
2017-01-01 13:59:40 -0500

    Title: Handlebars bugs, fixed but hacked. Body: HB causing errors site wide. Sinatra yields seems to allow ids from other templates to be called mysteriously when on non-related pages. This messes up HB. Turned all HB temlplates in ajax and fixed the problem. HB was cutting out index JQ styles. HB errors still visible across app. I suspect it is a Sinara problem.

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
 
-//   var blogTemplateString = $('#blog-template').html();
-//   // console.log(workTemplateString)
-// // // // 2.Compile the string into a function
-//   var blogTemplateRenderer = Handlebars.compile(blogTemplateString);
-// //       // console.log(workTemplate)
-// // // // 3.Input response into the function
-//   var output = blogTemplateRenderer()
-//   // console.log(output)
-// //
-// // //4.append to page
-//   $('#work-details').html(output);
+  $.ajax({
+    url:"js/json/blog_posts.json",
+    method:'GET',
+    data:{},
+    dataType:'json'
+  }).done(function(response){
 
+  var blogTemplateString = $('#blog-template').html();
+  // console.log(workTemplateString)
+// // // 2.Compile the string into a function
+  var blogTemplateRenderer = Handlebars.compile(blogTemplateString);
+//       // console.log(workTemplate)
+// // // 3.Input response into the function
+  var output = blogTemplateRenderer(response)
+  //   // console.log(output)
+// //
+// 4.append to page
+  $('#blog-details').html(output);
+  });
 });
