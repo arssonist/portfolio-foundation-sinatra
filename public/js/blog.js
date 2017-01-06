e4558b836f3bb7d8dd7bdea1f6253d6713e49c38
Your Name <arssonist@yahoo.com>
2017-01-05 19:26:09 -0500

    title:add other blogs to template. Body: add new json files for other blogs. Create new templates and new calls to the templates in ajax. Readjusted index but is now out of whack.

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,22 +1,39 @@
 $(document).ready(function(){
 
   $.ajax({
-    url:"js/json/blog_posts.json",
+    url:"js/json/code_blog_posts.json",
     method:'GET',
     data:{},
     dataType:'json'
   }).done(function(response){
 
-  var blogTemplateString = $('#blog-template').html();
+  var codeBlogTemplateString = $('#code_blog-template').html();
   // console.log(workTemplateString)
 // // // 2.Compile the string into a function
-  var blogTemplateRenderer = Handlebars.compile(blogTemplateString);
+  var codeBlogTemplateRenderer = Handlebars.compile(codeBlogTemplateString);
 //       // console.log(workTemplate)
 // // // 3.Input response into the function
-  var output = blogTemplateRenderer(response)
+  var output = codeBlogTemplateRenderer(response)
   //   // console.log(output)
 // //
 // 4.append to page
-  $('#blog-details').html(output);
+  $('#code_blog-details').html(output);
+
+
+  var filmBlogTemplateString = $('#film_blog-template').html();
+
+  var filmBlogTemplateRenderer = Handlebars.compile(filmBlogTemplateString);
+
+  var output = filmBlogTemplateRenderer(response)
+
+  $('#nothing_blog-details').html(output);
+  var nothingBlogTemplateString = $('#nothing_blog-template').html();
+
+  var nothingBlogTemplateRenderer = Handlebars.compile(nothingBlogTemplateString);
+
+  var output = nothingBlogTemplateRenderer(response)
+
+  $('#nothing_blog-details').html(output);
+
   });
 });
