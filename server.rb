6b9feb297541ae00746ec14c25b0a32e062e0178
Chris Del <drumgod101@gmail.com>
2018-10-11 15:42:45 -0400

    add mailer but getting timeout error. Until fixed comment out;

--- server.rb
+++ server.rb
@@ -1,28 +1,55 @@
 require 'sinatra'
-
 require 'newrelic_rpm'
-
+require 'dotenv/load'
 require 'sinatra/contrib'
+require 'Pony'
+require 'resolv-replace'
 configure { set :server, :puma }
 
-
 # SITE GLOBAL CONSTANTS/////////////
-$my_email = "drumgod101@gmail.com"
-$my_github = "https://github.com/arssonist/"
-$my_twitter = "https://twitter.com/The_Seperator"
-$my_phone = "306-519-0138"
-$my_blog = "https://chrisdelwebdev.wordpress.com/"
-$my_city = "Toronto, ON"
-$my_linkedin = "https://ca.linkedin.com/in/chrisdel1"
+$my_email = 'drumgod101@gmail.com'
+$my_github = 'https://github.com/arssonist/'
+$my_twitter = 'https://twitter.com/The_Seperator'
+$my_phone = '306-519-0138'
+$my_blog = 'https://chrisdelwebdev.wordpress.com/'
+$my_city = 'Toronto, ON'
+$my_linkedin = 'https://ca.linkedin.com/in/chrisdel1'
 
 get '/' do
-  erb :work
+ erb :work
 end
 
 get '/work' do
-  erb :work
+ erb :work
 end
 
 get '/contact' do
-  erb :contact
+ erb :contact
 end
+#
+# post '/contact' do
+#  name = params[:name]
+#  email = params[:email]
+#  message = params[:message]
+#  puts(ENV['MAIL_SERVER'])
+#
+#
+#  Pony.mail({
+#    :to => 'arssonist@yahoo.com',
+#    :via => :smtp,
+#    :via_options => {
+#      :address        => 'smtp.gmail.com',
+#      :port           => '587',
+#      :ssl => true,
+#      :user_name      => 'mote2zart@gmail.com',
+#      :password       => 'udykkvxnfuwvuxoz',
+#      :authentication => :plain, # :plain, :login, :cram_md5, no auth by default
+#      :domain         => "localhost.localdomain" # the HELO domain provided by the client to the server
+#    }
+#
+#  })
+#  puts('after')
+#
+#
+#  return redirect '/'
+# end
