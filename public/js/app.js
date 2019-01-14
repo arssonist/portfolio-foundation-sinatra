cdcc8477aea0804f4c36b4e6c155d25b9d3b65da
Chris Del <drumgod101@gmail.com>
2019-01-13 21:28:13 -0500

    add scroll down with button

--- public/js/app.js
+++
@@ -1,6 +0,0 @@
-// $(document).ready(function(){
-//   $('#test-button').on('click', function(){
-//     alert('test')
-//   })
-// });
-$(document).foundation()
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-    
-});
+function scrollIndex(){
+    // return window.scrollBy(0, 700)
+    console.log('test')
+    window.scrollTo({ top: 800, behavior: 'smooth' })
+}
+
+let button = document.querySelector(".hero-index>.button")
+button.addEventListener('click', scrollIndex)
